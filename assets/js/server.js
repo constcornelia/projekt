51bf893cce4fdbff3ad5c7cfeca805361969657f
constcornelia <c2ronnemyr@gmail.com>
2026-05-11 21:22:23 +0200

    cookie + server fix

--- assets/js/server.js
+++ assets/js/server.js
@@ -1,16 +1,134 @@
-import { serveFile } from "jsr:@std/http/file-server";
+import { serveFile, serveDir } from "jsr:@std/http/file-server";
+import { filterPlaylistsByTag } from "./playlists.js";
 
-// till för att lägga till aktiva och ta bort inaktiva users
-let cookies = [];
+const data = JSON.parse(Deno.readTextFileSync("../data/database.json"));
+const userData = JSON.parse(Deno.readTextFileSync("../data/users.json"));
 
- // ger ut random cookie
-function createRandomCookie () {
-    return crypto.randomUUID();
+const cookies = []; // Alla aktiva cookies ska sparas här
+
+// Skapar ett random cookieId
+function createRandomCookie () { // Ska denna kanske flyttas till ui eller api eller något... hmmmmm
+    return crypto.randomUUID(); 
+}
+
+async function handler(request) {
+    let url = new URL(request.url);
+
+    let songs = data.songs;
+    let playlists = data.playlists;
+    const users = userData.users;
+    
+    if (url.pathname == "/") {
+        const activeCookie = request.headers.get("cookie");
+    
+        // Kollar om det finns en aktiv cookie som matchar med en från minnet
+        let session = null;
+        for (let cookie of cookies) {
+            if (activeCookie != null && activeCookie.includes(cookie.cookie)) {
+                session = cookie;
+                break;
+            }
+        }
+
+        //  Om med finns någon kommer man till start...
+        if (session) { 
+            return serveFile(request, "../../test2.html");
+        }
+        
+        // ... annars kommer man till login
+        let options = {
+            status: 303,
+            headers: { "Location": "/login" }
+        }
+        return new Response("Unauthorized", options);
+    }
+    
+    // Logga in
+    if (url.pathname == "/login") {
+
+        if (request.method == "GET") {
+            return serveFile(request, "../../test.html")
+        }
+
+        if (request.method == "POST") {
+            let loginReq = await request.json();
+
+            // Kollar om den inloggade användaren finns i users.json
+            let loggedInUser = null;
+            for (let user of users) {
+                if (user.username == loginReq.username && user.password == loginReq.password) {
+                    loggedInUser = user;
+                    break;
+                }
+            }
+
+            if (!loggedInUser) {
+                return new Response("Wrong login", { status: 401 });
+            }
+
+            // Kollar i den aktiva cookie-arrayen om usern finns det
+            let existing = null;
+            for (let cookie of cookies) {
+                if (cookie.username == loggedInUser.username) {
+                    existing = cookie;
+                    break;
+                }
+            }
+            
+            // Skapar cookie-id och lägger in den i minnet
+            let cookieId = createRandomCookie();
+            if (existing) {
+                existing.cookie = cookieId;
+            } else {
+                cookies.push({ 
+                    username: loginReq.username,
+                    cookie: cookieId
+                });
+            }
+
+            let headers = {
+                "Set-Cookie": "sessionId=" + cookieId + "; Max-Age=10080" 
+            };
+            return new Response("Welcome!", { headers: headers });
+        }
+    }
+
+    if (url.pathname == "/logout" && request.method == "GET") {
+        let options = {
+            status: 303,
+            headers: {
+                "Location": "/login",
+                "Set-Cookie": "session_id=deleted; Max-Age=0"
+            }
+        };
+        return new Response(null, options);
+    }
+
+
+
+
+    if (request.method == "GET") {
+        if (url.pathname == "/") {
+            let tag = url.searchParams.get("tags");
+            if (tag) playlists = filterPlaylistsByTags(playlists, tag);
+        }
+    }
+
+
+
+    // if (request.method == "OPTIONS") {
+    //     return new Response(null, {
+    //         headers: headers
+    //     });
+    // }
+
 }
 
 // tar hand om cookies
 async function handleCookies (request) {
     let url = new URL (request.url);
+    let cookies = [];
+
     
     // går till startsidan
     if (request.method == "GET" && url.pathname == "/") {
@@ -34,7 +152,9 @@ async function handleCookies (request) {
         for (let user of users) {
             if (user.username == body.username && user.password == body.password) {
                 // loggedInUser = user; 
+                cookies.push(user)
                 let cookie = createRandomCookie();
+                console.log(cookies);
                 let headers = { 
                     "Content-Type": "text/html",
                     "Set-Cookie": "sessionId=" + cookie + "; Max-Age=10080" 
@@ -42,6 +162,17 @@ async function handleCookies (request) {
                 return new Response("Hello", headers);
             }
         }
+        console.log(cookies);
+
+        if (url.pathname == "/" && request.method == "GET") {
+            const activeCookie = request.headers.get("cookie");
+
+            for (let cookie of cookies) {
+
+            }
+        }
+
+
 
         // om användaren inte är utloggad
 /*         if (loggedInUser != null) {
@@ -52,13 +183,12 @@ async function handleCookies (request) {
             // Deno.writeTextFileSync("../data/users.json", JSON.stringify(users, null, 2));
 
             return new Response(await Deno.readTextFileSync("../../test.html"), {
-                headers: { // note to self: testa sätta detta separat och sedan return new response
+                headers: { 
                     "Content-Type": "text/html",
                     "Set-Cookie": "sessionId=" + cookie + "; Max-Age=10080" // 7hours, rework to 1 week? in seconds.
                 }
             });
         }
-        console.log("hej");
         cookies.push(loggedInUser);
 
         // cookies[username] = loggedInUser.username;
@@ -67,8 +197,51 @@ async function handleCookies (request) {
     }
 
 }
-Deno.serve(handleCookies);
 
+
+
+
+//     let playlists = data.playlists;
+//     let songs = data.songs;
+    
+//     if (request.method == "GET") {
+        
+//         // Get all playlists
+//         if (url.pathname == "/") {
+//             return serveFile(request, "../../test.html");
+//             // Kolla först att det finns en aktiv cookie - annars skicka till login
+
+//             // Filter playlist (by tags)
+//             // let tag = url.searchParams.get("tag");
+//             // if (tag) playlists = filterByTags(playlists, tags);
+
+//             // Sort by amount of tags, descending or ascending
+//             // let likesDesc = url.searchParams.get("likesDesc"); 
+//             // if (likesDesc == true) playlists = sortByLikes(playlists, true);
+//             // if (likesDesc == false) playlists = sortByLikes(playlists, false);
+//         }
+//         // Get playlists by search (through name and description)
+//         // Get one specific playlist by id
+//         // Get song by search (title or artist)
+//         // Get your own user info
+//     }
+
+//     if (request.method == "POST") {
+//         // Logging in or joining
+//         // Post your own playlist
+//     }
+
+//     if (request.method == "PATCH") {
+//         // Edit your own playlist (basic info such as name, description of tags) by id
+//         // Add songs to others playlists by playlist id and song id
+//     }
+
+//     if (request.method == "DELETE") {
+//         // Delete playlist by id (detta sker från ens profil)
+//     }
+// }
+
+Deno.serve(handler);
 
 // 1. loop igenom arrayen "cookies"
 
