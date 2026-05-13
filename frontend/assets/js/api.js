7cde343eab9d95615e4ad30319620ba00aa88f26
dilarak9 <dilarakarakocek@gmail.com>
2026-05-13 13:42:06 +0200

    Update api.js

--- frontend/assets/js/api.js
+++ frontend/assets/js/api.js
@@ -1,77 +1,119 @@
 const url = "http://localhost:8000/";
 
-let accept = { "Accept": "application/json" };
-let contentType = { "Content-Type": "application/json" };
+let authorization = {
+    "Authorization": "Bearer 67"
+}
 
-// Test om det går att göra alla requests med en metod
+let accept = { 
+    "Accept": "application/json" 
+};
+
+let contentType = { 
+    "Content-Type": "application/json" 
+};
+
 class API {
-    async requests(method, body, enpoint, msg) {
-        let options; 
-
-        if (method == "GET") options = { method: method, headers: { authorization, accept }};
-        if (method == "POST" || method == "PATCH") options = { method: method, headers: { authorization, contentType }, body: JSON.stringify(body) };
-        if (method == "DELETE") options = { method: method, headers: { authorization }};
-
+    async getRequest(endpoint) {
         try {
-            const request = new Request(url + enpoint, { options });
-            const response = await fetch(request);
-            if (!response.ok) throw new Error(response.status);
-    
+            let request = new Request (url + endpoint, {
+                method: "GET",
+                headers: {
+                    authorization,
+                    accept
+                }
+            });
+        
+            let response = await fetch (request);
+        
+            if(!response.ok) {
+                throw new Error (response.status);
+            }
+        
             let data = await response.json();
-            // Använd msg-argummentet för att potentiellt meddela användare specifikt om error eller success
-            return data;
-        } catch(error) {}
+
+            return data; 
+
+        } catch (error) {
+            main.innerHTML = "Couldn't reach data. Please try again! " + error;
+        } 
     }
 
-/*
-    // Method for all get-requests
-     async getRequest(endpoint) {
+    async postRequest (endpoint, body) {
         try {
-            const request = new Request(url + endpoint, { headers });
-            const response = await fetch(request);
-            if (!response.ok) throw new Error(response.status);
-
-            let data = await response.json();
-            return data;
-
-        } catch(error) {
-            // Handle error on frontend
-        }
-    }
-
-    // Method for all post-requests
-    async postRequest(endpoint, body) { // Get body from frontend
-        try {
-            const request = new Request(url + endpoint, {
+            let request = new Request (url + endpoint, {
                 method: "POST",
-                headers,
+                headers: {
+                    authorization,
+                    contentType
+                },
                 body: JSON.stringify(body)
             });
-            const response = await fetch(request);
-            if (!response.ok) throw new Error(response.status);
-            // Maybe an alert message to communicate that it was successfull to user
 
-            let data = await response.json();
-            return data; // Maybe needs to me made sure if it's the right response.status
-        } catch(error) {}
+            let response = await fetch (request);
+
+            let data = null; 
+
+            if (response.status !== 201) {
+                data = await response.json();
+                return data;
+            } 
+
+            alert("Successfully posted!");
+ 
+        } catch (error) {
+            main.innerHTML = "Couldn't post product " + error;
+        } 
     }
 
-    // Method for all patch-requests
-    async patchRequest(endpoint, body) {
+    async patchRequest (endpoint, body) {
         try {
-            const request = new Request(url + endpoint, {
+            let request = new Request (url + endpoint, {
                 method: "PATCH",
-                headers,
+                headers: {
+                    authorization,
+                    contentType
+                },
                 body: JSON.stringify(body)
             });
-            const response = await fetch(request);
-            if (!response.ok) throw new Error(response.status);
 
-            let data = await response.json();
-            return data; // Maybe needs to me made sure if it's the right response.status
-        } catch(error) {}
+            let response = await fetch (request);
+
+            let data = null; 
+
+            if (response.status !== 204) {
+                data = await response.json();
+                return data;
+            } 
+
+            alert("Successfully edited!");
+
+        } catch (error) {
+            main.innerHTML = "Couldn't edit product " + error;
+        } 
     }
 
-    // Method for all delete-requests
-    async deleteRequest(enpoint) {} */
+    async deleteRequest (endpoint) {
+        try {
+            let request = new Request (url + endpoint, {
+                method: "DELETE",
+                headers: {
+                    authorization
+                },
+            });
+
+            let response = await fetch (request);
+
+            let data = null; 
+
+            if (response.status !== 204) {
+                data = await response.json();
+                return data;
+            } 
+
+            alert("Successfully deleted!");
+            
+        } catch (error) {
+            main.innerHTML = "Couldn't delete product " + error;
+        } 
+    }
 }
