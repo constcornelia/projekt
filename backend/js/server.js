2ac5f6cbba65cf19fe8bebec6d4daa3cde1fab80
constcornelia <c2ronnemyr@gmail.com>
2026-05-13 19:12:48 +0200

    fix cookie

--- backend/js/server.js
+++ backend/js/server.js
@@ -19,7 +19,7 @@ async function handler(request) {
     const users = userData.users;
 
     
-    if (url.pathname == "/") {
+    if (url.pathname == "/" && request.method == "GET") {
         const activeCookie = request.headers.get("cookie");
     
         // Kollar om det finns en aktiv cookie som matchar med en från minnet
@@ -33,20 +33,26 @@ async function handler(request) {
 
         //  Om med finns någon kommer man till start...
         if (session) { 
+            console.log("hej");
             return serveFile(request, "../../frontend/main.html");
         }
         
         // ... annars kommer man till login
         let options = {
-            status: 303,
-            headers: { "Location": "/welcome" }
+            "Location": "/welcome",
+            status: 303
         }
+
         return new Response("Unauthorized", options);
     }
 
     if (url.pathname == "/welcome" && request.method == "GET") {
         return serveFile(request, "../../frontend/intro.html");
     }
+
+    // if (url.pathname == "/signup") {
+    //     return serveFile(request, "../../frontend/signup.html")
+    // }
     
     // Logga in
     if (url.pathname == "/login") {
@@ -57,7 +63,6 @@ async function handler(request) {
 
         if (request.method == "POST") {
             let loginReq = await request.json();
-            console.log(loginReq)
 
             // Kollar om den inloggade användaren finns i users.json
             let loggedInUser = null;
@@ -94,10 +99,18 @@ async function handler(request) {
 
             // Skapar cookien
             let headers = {
-                "Set-Cookie": "sessionId=" + cookieId + "; Max-Age=10080" 
+                "Set-Cookie": "sessionId=" + cookieId + "; Max-Age=10080",
+                "Location": "/"
             };
 
-            return new Response("Welcome!", { headers: headers });
+
+
+            // return serveFile(request, "../../frontend/main.html");
+
+            return new Response(null, { 
+                status: 303,
+                headers: headers 
+            });
         }
     }
 
