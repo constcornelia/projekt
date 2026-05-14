074209373d20e983b71bf5db1b0fc8b29bf8fbbf
NanpaMeli <khusanova.elena@gmail.com>
2026-05-14 03:16:18 +0200

    nya elemented add delete edit playlist

--- frontend/assets/js/ui.js
+++ frontend/assets/js/ui.js
@@ -1,19 +1,17 @@
+//det verkar att search funkar inte utan submit
+// så  gjorde jag två former for search och sortering
+//for main.html
 let FilterForm = document.getElementById("FilterForm");
 FilterForm.addEventListener("change", function (event) {
-        console.log("new value!", event.target);
+    console.log("new value!", event.target);
 
-    })
+})
 
-    let SearchForm = document.getElementById("SearchForm");    
-    SearchForm.addEventListener("submit", function (event) {
-        event.preventDefault();
-        let SearchInput = SearchForm.SearchInput.value;
-        console.log("new value!", SearchForm.SearchInput);
+let AddSongForm = document.getElementById("AddSongForm");
+AddSongForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let SearchInput = SearchForm.SearchInput.value;
+    console.log("new value!", SearchForm.SearchInput);
 
-    })
+})
 
-
-    //det verkar att search funkar inte utan submit
-    // så  gjorde jag två former for search och sortering
-
-    
