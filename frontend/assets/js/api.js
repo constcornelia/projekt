4928c3d7ff9d874d6d2cf0d6a342abb7bb419fdf
dilarak9 <dilarakarakocek@gmail.com>
2026-05-15 12:11:38 +0200

    update up and api

--- frontend/assets/js/api.js
+++ frontend/assets/js/api.js
@@ -1,119 +1,144 @@
 const url = "http://localhost:8000/";
 
+
 let authorization = {
-    "Authorization": "Bearer 67"
+   "Authorization": "Bearer 67"
 }
 
-let accept = { 
-    "Accept": "application/json" 
+
+let accept = {
+   "Accept": "application/json"
 };
 
-let contentType = { 
-    "Content-Type": "application/json" 
+
+let contentType = {
+   "Content-Type": "application/json"
 };
+
 
 class API {
-    async getRequest(endpoint) {
-        try {
-            let request = new Request (url + endpoint, {
-                method: "GET",
-                headers: {
-                    // authorization,
-                    accept
-                }
-            });
-        
-            let response = await fetch (request);
-        
-            if(!response.ok) {
-                throw new Error (response.status);
-            }
-        
-            let data = await response.json();
+   async getRequest(endpoint) {
+       try {
+           let request = new Request (url + endpoint, {
+               method: "GET",
+               headers: {
+                   // authorization,
+                   accept
+               }
+           });
+      
+           let response = await fetch (request);
+      
+           if(!response.ok) {
+               throw new Error (response.status);
+           }
+      
+           let data = await response.json();
 
-            return data; 
 
-        } catch (error) {
-            main.innerHTML = "Couldn't reach data. Please try again! " + error;
-        } 
-    }
+           return data;
 
-    async postRequest (endpoint, body) {
-        try {
-            let request = new Request (url + endpoint, {
-                method: "POST",
-                headers: {
-                    authorization,
-                    contentType
-                },
-                body: JSON.stringify(body)
-            });
 
-            let response = await fetch (request);
+       } catch (error) {
+           main.innerHTML = "Couldn't reach data. Please try again! " + error;
+       }
+   }
 
-            let data = null; 
 
-            if (response.status !== 201) {
-                data = await response.json();
-                return data;
-            } 
+   async postRequest (endpoint, body) {
+       try {
+           let request = new Request (url + endpoint, {
+               method: "POST",
+               headers: {
+                   authorization,
+                   contentType
+               },
+               body: JSON.stringify(body)
+           });
 
-            alert("Successfully posted!");
- 
-        } catch (error) {
-            main.innerHTML = "Couldn't post product " + error;
-        } 
-    }
 
-    async patchRequest (endpoint, body) {
-        try {
-            let request = new Request (url + endpoint, {
-                method: "PATCH",
-                headers: {
-                    authorization,
-                    contentType
-                },
-                body: JSON.stringify(body)
-            });
+           let response = await fetch (request);
 
-            let response = await fetch (request);
 
-            let data = null; 
+           let data = null;
 
-            if (response.status !== 204) {
-                data = await response.json();
-                return data;
-            } 
 
-            alert("Successfully edited!");
+           if (response.status !== 201) {
+               data = await response.json();
+               return data;
+           }
 
-        } catch (error) {
-            main.innerHTML = "Couldn't edit product " + error;
-        } 
-    }
 
-    async deleteRequest (endpoint) {
-        try {
-            let request = new Request (url + endpoint, {
-                method: "DELETE",
-                headers: {
-                    authorization
-                },
-            });
+           alert("Successfully posted!");
+       } catch (error) {
+           main.innerHTML = "Couldn't post product " + error;
+       }
+   }
 
-            let response = await fetch (request);
 
-            let data = null; 
+   async patchRequest (endpoint, body) {
+       try {
+           let request = new Request (url + endpoint, {
+               method: "PATCH",
+               headers: {
+                   authorization,
+                   contentType
+               },
+               body: JSON.stringify(body)
+           });
 
-            if (response.status !== 204) {
-                data = await response.json();
-                return data;
-            } 
 
-            alert("Successfully deleted!");
-            
-        } catch (error) {
-            main.innerHTML = "Couldn't delete product " + error;
-        } 
-    }
+           let response = await fetch (request);
+
+
+           let data = null;
+
+
+           if (response.status !== 204) {
+               data = await response.json();
+               return data;
+           }
+
+
+           alert("Successfully edited!");
+
+
+       } catch (error) {
+           main.innerHTML = "Couldn't edit product " + error;
+       }
+   }
+
+
+   async deleteRequest (endpoint) {
+       try {
+           let request = new Request (url + endpoint, {
+               method: "DELETE",
+               headers: {
+                   authorization
+               },
+           });
+
+
+           let response = await fetch (request);
+
+
+           let data = null;
+
+
+           if (response.status !== 204) {
+               data = await response.json();
+               return data;
+           }
+
+
+           alert("Successfully deleted!");
+          
+       } catch (error) {
+           main.innerHTML = "Couldn't delete product " + error;
+       }
+   }
 }
+
+
+
+
--- frontend/assets/js/ui.js
+++ frontend/assets/js/ui.js
@@ -1,76 +1,96 @@
+
 // Product   =   Playlist
 // Brand     =   Song
 // Category  =   Tags
 
+
 const main = document.querySelector("main");
+
 
 let api = new api();
 
+
 class UI {
-   async getPlaylists() {
-       let playlists = await api.getRequest("/api/playlists");
-       this.renderPlaylists(playlists);
-   }
+  async getPlaylists() {
+      let playlists = await api.getRequest("/api/playlists");
+      this.renderPlaylists(playlists);
+  }
 
-   async getSongs() {
-       let songs = await api.getRequest("/")
-       this.renderSongs(songs);
-   }
 
-   async renderPlaylists (playlists) {
-       main.innerHTML = "";
+  async getSongs() {
+      let songs = await api.getRequest("/")
+      this.renderSongs(songs);
+  }
 
-       let users = await api.getRequest("/");
 
-       for (let playlist of playlists) {
-           let a = document.createElement("a");
+  async renderPlaylists (playlists) {
+      main.innerHTML = "";
 
-           a.href = `personal.html?id=${playlist.id}`;
 
-           let ownerName;
+      let users = await api.getRequest("/");
 
-        for (let user of users) {
-            if (user.id == playlist.ownerId) {
-                ownerName = user.username;
-            }
-       }
-           a.innerHTML = `
-           <p><span>Likes</span>: ${playlist.likes}</p>
-           <img src="${playlist.coverImgUrl}"></img>
-           <h2>${playlist.name}</h2>
-           <p><span>Owner</span>: ${ownerName}</p>
-           <p><span>Tags</span>: #${playlist.tags}</p>
-           <p>${playlist.description}</p>
-           `;
 
-           main.append(a);
-       }
-   }
+      for (let playlist of playlists) {
+          let a = document.createElement("a");
 
-   async renderSongs (songs) {
-       main.innerHTML = "";
 
-       for (let song of songs) {
-           let div = document.createElement("div");
+          a.href = `personal.html?id=${playlist.id}`;
 
-           div.innerHTML = `
-           <img src="${song.coverImgUrl}"></img>
-           <p>${song.name}</p>
-           <p>${song.artist}</p>
-           `;
 
-           main.appendChild(div);
-       }
-   }
+          let ownerName;
 
-   async dropDownsPlaylist (playlistElement) {
-       let playlists = await api.getRequest("/")
 
-       for (let playlist of playlists) {
-           const option = document.createElement("option");
-           option.value = playlist.id;
-           option.textContent = playlist.name;
-           playlistElement.append(option);
-       }
-   }
+       for (let user of users) {
+           if (user.id == playlist.ownerId) {
+               ownerName = user.username;
+           }
+      }
+          a.innerHTML = `
+          <p><span>Likes</span>: ${playlist.likes}</p>
+          <img src="${playlist.coverImgUrl}"></img>
+          <h2>${playlist.name}</h2>
+          <p><span>Owner</span>: ${ownerName}</p>
+          <p><span>Tags</span>: #${playlist.tags}</p>
+          <p>${playlist.description}</p>
+          `;
+
+
+          main.append(a);
+      }
+  }
+
+
+  async renderSongs (songs) {
+      main.innerHTML = "";
+
+
+      for (let song of songs) {
+          let div = document.createElement("div");
+
+
+          div.innerHTML = `
+          <img src="${song.coverImgUrl}"></img>
+          <p>${song.name}</p>
+          <p>${song.artist}</p>
+          `;
+
+
+          main.appendChild(div);
+      }
+  }
+
+
+  async dropDownsPlaylist (playlistElement) {
+      let playlists = await api.getRequest("/")
+
+
+      for (let playlist of playlists) {
+          const option = document.createElement("option");
+          option.value = playlist.id;
+          option.textContent = playlist.name;
+          playlistElement.append(option);
+      }
+  }
 }
+
+
