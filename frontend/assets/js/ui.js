03074f34273c792bc3e28cf363668ac3092240d0
NanpaMeli <khusanova.elena@gmail.com>
2026-05-18 19:18:43 +0200

    kopplar ny still i main.html till ui.js

--- frontend/assets/js/ui.js
+++ frontend/assets/js/ui.js
@@ -1,6 +1,7 @@
 //det verkar att search funkar inte utan submit
 // så  gjorde jag två former for search och sortering
 //for main.html
+/*
 let FilterForm = document.getElementById("FilterForm");
 FilterForm.addEventListener("change", function (event) {
     console.log("new value!", event.target);
@@ -13,5 +14,93 @@ AddSongForm.addEventListener("submit", function (event) {
     let SearchInput = SearchForm.SearchInput.value;
     console.log("new value!", SearchForm.SearchInput);
 
-})
+})*/
+
+// Product   =   Playlist
+// Brand     =   Song
+// Category  =   Tags
+const section = document.querySelector("#PublicPlaylistsCollection");
+
+let api = new API();
+
+class UI {
+
+  async getPlaylists() {
+      let playlists = await api.getRequest("/api/playlists");
+      this.renderPlaylists(playlists);
+  }
+
+  async getSongs() {
+      let songs = await api.getRequest("/api/songs")
+      this.renderSongs(songs);
+  }
+
+  async renderPlaylists(playlists) {
+
+    section.innerHTML = "";
+
+    let users = await api.getRequest("/api/users");
+    console.log(users);
+
+      for (let playlist of playlists) {
+
+          let a = document.createElement("a");
+
+          a.href = `personal.html?id=${playlist.id}`;
+
+          let ownerName;
+
+        for (let user of users) {
+            if (user.id == playlist.ownerId) {
+                ownerName = user.username;
+            }
+        }
+        a.innerHTML = `
+        <div class="playlist-card">
+          <h1 class="title-UI">${playlist.name}
+          </h1>
+          <div class="playlist-actions">
+            <button class="LikePlaylist-Button system-UI"><span>${playlist.likes.length}</span></button>
+            <button class="Play StopPlaying"></button>
+          </div>
+          <p class="system-UI-accent"><span>Tags</span>:#${playlist.tags}</p>
+          <img src="${playlist.imgUrl}">
+        </div>
+        `;
+          section.appendChild(a);
+      }
+  }
+
+  async renderSongs (songs) {
+      section.innerHTML = "";
+
+      for (let song of songs) {
+          let div = document.createElement("div");
+
+        div.innerHTML = `
+        //   <img src="${song.coverImgUrl}"></img>
+          <p>${song.name}</p>
+          <p>${song.artist}</p>
+          `;
+
+          section.appendChild(div);
+      }
+  }
+
+
+  async dropDownsPlaylist (playlistElement) {
+    let playlists = await api.getRequest("/api/playlists");
+
+      for (let playlist of playlists) {
+          const option = document.createElement("option");
+          option.value = playlist.id;
+          option.textContent = playlist.name;
+          playlistElement.append(option);
+      }
+  }
+}
+
+const ui = new UI();
+ui.getPlaylists();
+
 
