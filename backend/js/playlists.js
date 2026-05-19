d18a3756b163864a0a42a0ad0e1b773b6817a48c
dilarak9 <dilarakarakocek@gmail.com>
2026-05-19 17:39:22 +0200

    raderade dubbletter och fixade buggar

--- backend/js/playlists.js
+++ backend/js/playlists.js
@@ -21,7 +21,7 @@ export function getPlaylistBySearch(database, query) {
     let q = query.toLowerCase();
 
     for (let playlist of database.playlists) {
-        if (playlist.name.toLowerCase().icludes(q) || playlist.tags.toLowerCase().icludes(q) || playlist.description.toLowerCase().icludes(q)) {
+        if (playlist.name.toLowerCase().includes(q) || playlist.tags.includes(q) || playlist.description.toLowerCase().includes(q)) {
             filteredPlaylists.push(playlist);
         }
     }
@@ -63,40 +63,6 @@ export function getTags(playlists) {
     return tags;
 }
 
-// export function createPlaylistId (database) {
-//     let highestId = 0;
-
-//     for (let playlist of database.playlists) {
-//         let id = parseInt(playlist.id.split("-")[1]);
-
-//         if (id > highestId) {
-//             highestId = id;
-//         }
-//     }
-//     return `p-${highestId + 1}`;
-// }
-
-// export function createPlaylistById (database, body) {
-//     let playlists = database.playlists;
-
-//     let newId = createPlaylistId(database);
-
-//     let newPlaylist = {
-//         id: newId,
-//         // ownerId: "u-1", måste fixa getUser??
-//         name: body.name,
-//         description: body.description,
-//         imgUrl: body.imgUrl,
-//         tags: body.tags,
-//         songs: body.songs
-//     };
-//     playlists.push(newPlaylist);
-
-//     Deno.writeTextFileSync("database.json", JSON.stringify(database));
-
-//     return newPlaylist;
-// }
-
 export function deletePlaylistById(database, playlistId) {
     let updatedPlaylists = [];
 
@@ -170,7 +136,7 @@ export function editPlaylistById(database, playlistId, body) {
         }
     }
 
-    return tags;
+    return true;
 }
 
 export function createPlaylistId (database) {
@@ -205,88 +171,6 @@ export function createPlaylistById (database, body) {
     Deno.writeTextFileSync("database.json", JSON.stringify(database));
 
     return newPlaylist;
-}
-
-export function deletePlaylistById(database, playlistId) {
-    let updatedPlaylists = [];
-
-    let found = false;
-
-    for (let playlist of database.playlists) {
-        if (playlist.id === playlistId) {
-            found = true;
-        } else {
-            updatedPlaylists.push(playlist);
-        }
-    } 
-
-    if (!found) {
-        return false;
-    }
-
-    database.playlists = updatedPlaylists;
-
-    Deno.writeTextFileSync("database.json", JSON.stringify(database));
-
-    return true;
-}
-
-export function deleteSongFromPlaylist (playlists, playlistId, songId) {
-    for (let playlist of playlists) {
-
-        if (playlist.id === playlistId) {
-            let updatedSongs = [];
-
-            for (let song of playlist.songs) {
-
-                if (song.songId !== songId) {
-                    updatedSongs.push(song);
-                }
-
-            }
-            playlist.songs = updatedSongs;
-        }
-    }
-    return playlists;
-}
-
-export function editPlaylistById(database, playlistId, body) {
-    let found = false;
-
-    for (let playlist of database.playlists) {
-        if (playlist.id === playlistId) {
-
-            found = true;
-
-            if (body.name) {
-                playlist.name = body.name;
-            }
-
-            if (body.tags) {
-                playlist.tags = body.tags;
-            }
-
-            if (body.songs) {
-                playlist.songs = body.songs;
-            }
-
-            if (body.imgUrl) {
-                playlist.imgUrl = body.imgUrl;
-            }
-
-            if (body.description) {
-                playlist.description = body.description;
-            }
-        }
-    }
-
-    if (!found) {
-        return false;
-    }
-
-    Deno.writeTextFileSync("database.json", JSON.stringify(database));
-
-    return true;
 }
 
 export function sortPlaylistsByLikes(playlists) {
