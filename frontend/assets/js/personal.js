9cb59fdda136ce59823d03ea99b7436cc9c0b811
constcornelia <c2ronnemyr@gmail.com>
2026-05-22 23:58:56 +0200

    Update personal.js

--- frontend/assets/js/personal.js
+++ frontend/assets/js/personal.js
@@ -10,6 +10,7 @@
 //     createPlaylist.style.display = "none";
 // });
 
+<<<<<<< Updated upstream
 console.log("carolina är konstig");
 const createPlaylistForm = document.querySelector("#CreatePlaylistForm");
 createPlaylistForm.addEventListener("submit", async function onSubmit(event) {
@@ -23,4 +24,32 @@ createPlaylistForm.addEventListener("submit", async function onSubmit(event) {
     };
     
     let response = await fetch("/new-playlist", options);
+});});
+=======
+// const createPlaylistForm = document.querySelector("#CreatePlaylistForm");
+// createPlaylistForm.addEventListener("submit", async function onSubmit(event) {
+//     event.preventDefault();
+
+//     const data = new FormData(createPlaylistForm);
+
+//     let options = {
+//         method: "POST",
+//         body: data
+//     };
+
+//     let response = await fetch("", options);
+// });
+
+const form = document.querySelector("#CreatePlaylistForm");
+form.addEventListener("submit", async function onSubmit(event) {
+    event.preventDefault();
 });
+
+// Få userId
+// Sök på låtar
+// Lägg till låtar
+// Flytta renderade låtar till added songs
+// Lägg in allt i databasen
+// Uppdatera och rendera om sina egna låtar
+// Alerta usern om något går fel eller om spellistan lyckades skapas
+>>>>>>> Stashed changes
