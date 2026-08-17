6de350a239caf2931edfb81c3a093165a4630f8d
dilarak9 <dilarakarakocek@gmail.com>
2026-08-17 13:40:05 +0200

    fix login, signup, edit

--- frontend/assets/js/signup.js
+++ frontend/assets/js/signup.js
@@ -1,7 +1,8 @@
 const form = document.querySelector("#signup");
+const errorAlert = document.querySelector(".error-note");
 form.addEventListener("submit", async function(event) {
     event.preventDefault();
-    
+    errorAlert.textContent = "";
     const data = new FormData(form);
 
     let options = {
@@ -11,8 +12,14 @@ form.addEventListener("submit", async function(event) {
 
     let response = await fetch("/signup", options);
 
-    const errorAlert = document.querySelector(".error-note");
-    if (response.status == 401) errorAlert.innerHTML = "Username is already taken";
-    if (response.status == 400) errorAlert.innerHTML = "Please enter a username AND a password";
+    if (response.status == 400) {
+        const result = await response.json();
+        if (result.message === "Profile picture is missing.") {
+            errorAlert.textContent = "Please choose a profile picture.";
+        } else if (result.message === "Not acceptable input data.") {
+            errorAlert.textContent = "Username or password is invalid, or username is already taken.";
+        }
+        return;
+    }
     if (response.status === 303 || response.ok) window.location.href = "/";
 });
