ca5edf586ceedec59b663fa7eb4b7c33d73f166e
dilarak9 <dilarakarakocek@gmail.com>
2026-08-17 15:51:39 +0200

    Update api.js

--- frontend/assets/js/api.js
+++ frontend/assets/js/api.js
@@ -51,7 +51,7 @@ class API {
        try {
            let request = new Request (url + endpoint, {
                method: "PATCH",
-               body: JSON.stringify(body)
+               body: body
            });
            let response = await fetch (request);
            if (!response.status) {
